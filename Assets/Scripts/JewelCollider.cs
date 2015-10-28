93536422666e6f86919bbf59c96ea0ed946b8736
michael <michael.genty@free.fr>
2015-10-28 16:29:51 +0100

    Added jwels + jewels UI counter

---
+++ Assets/Scripts/JewelCollider.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class JewelCollider : MonoBehaviour {
+
+   
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnTriggerEnter2D(Collider2D other) {
+        GameObject Jewel = GameObject.Find("JewelCounter");
+        JewelCounter jewelCounter = (JewelCounter)Jewel.GetComponent(typeof(JewelCounter));
+
+        GameObject Go = this.gameObject;
+        jewelCounter.addJewelCount();
+        Destroy(Go);
+    }
+}
---
+++ Assets/Scripts/JewelCounter.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class JewelCounter : MonoBehaviour {
+
+    Text textJewels;
+    float nbOfJewels = 0;
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void addJewelCount() {
+        nbOfJewels += 1;
+        GameObject.Find("nbOfJewels").GetComponent<Text>().text = nbOfJewels.ToString() + "x";
+    }
+
+}
